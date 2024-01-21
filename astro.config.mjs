ad54c915e2c74d348f022059fd60725afa061916
cactusmacoin <macoinpedro@gmail.com>
2024-01-21 16:16:56 -0300

    new c

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://cactusmacoin.github.io',
-  base: '/astro-landing'  
+  integrations: [tailwind(), react()]
 });
